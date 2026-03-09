579c99abcaaa9d6836cf1e75e182b0a56fe4adbe
jasminecole-spec <jasmine.cole@crpusd.org>
2026-03-09 08:52:12 -0700

    Add files via upload

---
+++ Numbers2D.java
@@ -0,0 +1,14 @@
+/*
+ * Activity 3.3.3
+*/
+public class Numbers2D
+{
+  public static void main(String[] args)
+  {
+    int[][] numbers = { {1,5,3,8,-3,0,3},
+                      {4,4,8,0,3,-1,1}, 
+                      {0,3,8,4,-2,7, 6} };
+
+
+  }
+}
