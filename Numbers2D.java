96a5e3fc099852a7193b03b84195f0cd1d1ebc27
Jasmine <jasmine.cole@crpusd.org>
2026-03-10 22:47:28 -0700

    sum and average

--- Numbers2D.java
+++ Numbers2D.java
@@ -9,6 +9,30 @@ public class Numbers2D
                       {4,4,8,0,3,-1,1}, 
                       {0,3,8,4,-2,7, 6} };
 
+    //SUM
+    int sum = 0;
+    for (int[] arr : numbers)
+    {
+      for (int index : arr)
+      {
+        sum += index;
+      }
+    }
+    System.out.println(sum);
 
+    //AVERAGE
+    int count = 0;
+    double average = 0.0;
+    double sum2 = 0.0;
+    for (int arr[] : numbers)
+    {
+    for (int index : arr)
+      {
+      sum2 += index;
+      count++;
+      }
+    }
+    average = (double)sum2/count;
+    System.out.println(average);
   }
 }
